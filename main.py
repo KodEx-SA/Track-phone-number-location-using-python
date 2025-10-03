a9c227c957d3638fecb54fd8134690b8641edc0f
Ashley K Motsie <motsieashley31@gmail.com>
2025-10-03 11:10:01 +0200

    main updates

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import phonemumbers
+import phonenumbers
 from phonenumbers import geocoder, carrier, timezone
 from phone import number
+
+pepnumber = phonenumbers.parse(number)
+location = geocoder.description_for_number(pepnumber, "en")
+service_provider = carrier.name_for_number(pepnumber, "en")
+time_zone = timezone.time_zones_for_number(pepnumber)
+print(location)
